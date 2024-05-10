4691bf4d7a39749d6ed049cc503414eb3952d618
bharatm29 <bharatmhes@gmail.com>
2024-05-10 15:50:21 +0530

    drawing and erasing works!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
+#include <functional>
 #include <iostream>
+#include <random>
 #include <raylib.h>
 #include <vector>
+
+using std::vector;
 
 #define WIDTH 800
 #define HEIGHT 600
 
 #define CELL_WIDTH 10
 #define CELL_HEIGHT CELL_WIDTH
+
+float gen_random_float(float min, float max) {
+    typedef std::mt19937 Engine;
+    typedef std::uniform_real_distribution<float> Distribution;
+
+    auto r =
+        std::bind(Distribution(0.0f, 1.0f), Engine((unsigned int)time(NULL)));
+    return r();
+}
 
 int main() {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
@@ -19,7 +32,7 @@ int main() {
 
     bool eraseMode = false;
 
-    std::vector<std::vector<int>> v(rows, std::vector<int>(cols, 0));
+    vector<vector<int>> v(rows, vector<int>(cols, 0));
 
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -36,24 +49,22 @@ int main() {
                 const int cellx = GetMouseX() / CELL_WIDTH;
                 const int celly = GetMouseY() / CELL_HEIGHT;
 
-                v[cellx][celly] = eraseMode ? 0 : 1;
+                v[celly][cellx] = eraseMode ? 0 : 1;
             }
 
             for (int r = 0; r < rows; r++) {
                 for (int c = 0; c < cols; c++) {
                     if (v[r][c] == 1) {
-                        Vector2 position = Vector2{.x = (float)CELL_WIDTH *
-                                                        r, // why r and not c?
-                                                   .y = (float)CELL_HEIGHT * c};
+                        Vector2 position = Vector2{.x = (float)CELL_WIDTH * c,
+                                                   .y = (float)CELL_HEIGHT * r};
 
                         Vector2 size =
                             Vector2{.x = CELL_WIDTH, .y = CELL_HEIGHT};
 
-                        DrawRectangleV(position, size, BEIGE);
+                        DrawRectangleV(position, size, RED);
 
-                        // FIXME: DrawRectangleLinesEx(Rectangle{.x =
-                        // position.x, .y = position.y, .width = CELL_WIDTH,
-                        // .height = CELL_HEIGHT}, 1.12, RED);
+                        // FIXME: DrawRectangleLines(0, 0, CELL_WIDTH,
+                        // CELL_HEIGHT, RED);
                     }
                 }
             }
