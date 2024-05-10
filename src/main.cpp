6a4c64d0f48b0eeccdbd8b65b3399f3b9670b490
bharatm29 <bharatmhes@gmail.com>
2024-05-10 14:08:26 +0530

    drawing the grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
+#include <iostream>
 #include <raylib.h>
+#include <vector>
 
-#define WIDTH 600
+#define WIDTH 800
 #define HEIGHT 600
+
+#define CELL_WIDTH 10
+#define CELL_HEIGHT CELL_WIDTH
 
 int main() {
     SetConfigFlags(FLAG_MSAA_4X_HINT);
@@ -9,16 +14,49 @@ int main() {
 
     SetTargetFPS(60);
 
-    if (!WindowShouldClose()) {
-        SetMousePosition(WIDTH / 2, HEIGHT / 2);
-    }
+    const int rows = HEIGHT / CELL_HEIGHT;
+    const int cols = WIDTH / CELL_WIDTH;
+
+    bool eraseMode = false;
+
+    std::vector<std::vector<int>> v(rows, std::vector<int>(cols, 0));
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
         {
             ClearBackground(BLACK);
-            DrawCircle(GetMouseX(), GetMouseY(), 20, RAYWHITE);
+            if (IsKeyDown(KEY_LEFT_SHIFT)) {
+                eraseMode = true;
+            } else {
+                eraseMode = false;
+            }
+
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                const int cellx = GetMouseX() / CELL_WIDTH;
+                const int celly = GetMouseY() / CELL_HEIGHT;
+
+                v[cellx][celly] = eraseMode ? 0 : 1;
+            }
+
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < cols; c++) {
+                    if (v[r][c] == 1) {
+                        Vector2 position = Vector2{.x = (float)CELL_WIDTH *
+                                                        r, // why r and not c?
+                                                   .y = (float)CELL_HEIGHT * c};
+
+                        Vector2 size =
+                            Vector2{.x = CELL_WIDTH, .y = CELL_HEIGHT};
+
+                        DrawRectangleV(position, size, BEIGE);
+
+                        // FIXME: DrawRectangleLinesEx(Rectangle{.x =
+                        // position.x, .y = position.y, .width = CELL_WIDTH,
+                        // .height = CELL_HEIGHT}, 1.12, RED);
+                    }
+                }
+            }
         }
 
         EndDrawing();
